7d9c2fd00381a44acbfd57c78581e2d52b965efc
Nolan Kovacik <noltron000@gmail.com>
2018-10-13 23:13:10 -0700

    removed annoying alert popup

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ tip_button.onclick = function() {
 	const subtotal = Number(get_subtotal.value)
 	const tip_value =  Number(subtotal * get_tip_rate.value / 100)
 	const total = Number(subtotal + tip_value)
-	alert(total)
 	get_tip_value.innerHTML = "$" + tip_value.toFixed(2)
 	get_total.innerHTML = "$" + total.toFixed(2)
 }
